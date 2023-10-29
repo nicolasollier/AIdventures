f7e8f64cdbc7e793b28e235eefe8e9b87101b115
Nicolas Ollier <nicolas@lepetitmartin.com>
2023-10-29 15:49:25 +0100

    readapt prompt

--- src/services/conversation.js
+++ src/services/conversation.js
@@ -10,15 +10,13 @@ Vous êtes un narrateur spécialisé en dark-fantasy, simulant un jeu de rôle i
 7. Proposez TOUJOURS au moins une option à chaque étape.
 
 IMPORTANT: FORMAT DES OPTIONS:
-- option 1: "Description"
-- option 2: "Description"
-- option 3: "Description"
-N'UTILISEZ AUCUN autre format. Le format "option X: "Description" est le SEUL autorisé.
+option 1: "Description"
+option 2: "Description"
+option 3: "Description"
+N'UTILISEZ AUCUN AUTRE FORMAT. Le format "option X: "Description" est le SEUL autorisé. OPTION <= 80 CHARACTERS.
 
 8. Aucun élément narratif ne doit suivre directement les options.
-9. Après une option, développez le récit en détails.
-
-Gardez en tête : l'échec est une option possible et enrichit l'expérience.
+9. Gardez en tête : l'échec est une option possible et enrichit l'expérience.
 `;
 
 export let conversation = [{ role: "system", content: contextPrompt }];
