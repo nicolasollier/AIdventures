a856c0da4fd079f2474f3ad1b042526d8f03c4dd
Nicolas Ollier <nicolas@lepetitmartin.com>
2023-11-02 09:34:07 +0100

    removes legacy file

--- src/services/cleanConversation.js
+++
@@ -1,9 +0,0 @@
-export function cleanConversation(conversation) {
-  return conversation.map((message) => {
-    if (message.role === "assistant") {
-      const { options, ...rest } = message;
-      return rest;
-    }
-    return message;
-  });
-}
