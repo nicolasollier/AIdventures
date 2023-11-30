e6312b0a497468855b865ee64d9eefdb9b13babd
Nicolas Ollier <nicolas@lepetitmartin.com>
2023-11-30 20:46:06 +0100

    set sentry for frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,36 @@
-//Dependencies
+// Dependencies
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 
-//Contexts
+// Contexts
 import { AuthProvider } from "./contexts/AuthContext.jsx";
 import { ConversationProvider } from "./contexts/ConversationContext.jsx";
 import { PlayerContextProvider } from "./contexts/PlayerContext.jsx";
 
-//Styles
+// Styles
 import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// Sentry Initialization
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    new Integrations.BrowserTracing({
+      tracePropagationTargets: ["localhost", "/api"],
+    }),
+    new Sentry.Replay(),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+// Render Application
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <ChakraProvider>
     <AuthProvider>
       <ConversationProvider>
