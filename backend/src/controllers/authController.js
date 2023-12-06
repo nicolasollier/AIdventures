299ff648e339d82e56ff960eaf902f27e1b41b14
Nicolas Ollier <nicolas@lepetitmartin.com>
2023-12-06 09:41:23 +0100

    change 401 message

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -4,9 +4,9 @@ const authController = {
   verifyToken: (req, res, next) => {
     const token = req.cookies["auth-token"];
 
-    // if (!token) {
-    //   return res.status(401).send("Access Denied");
-    // }
+    if (!token) {
+      return res.status(401).send("Access Denied: No token provided");
+    }
 
     try {
       const verified = jwt.verify(token, process.env.TOKEN_SECRET);
