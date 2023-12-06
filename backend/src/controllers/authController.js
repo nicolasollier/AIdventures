f762c17aa8d0d7fde599eebc017f7a00b83b0a5b
Nicolas Ollier <nicolas@lepetitmartin.com>
2023-12-06 09:35:55 +0100

    small fix

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -4,9 +4,9 @@ const authController = {
   verifyToken: (req, res, next) => {
     const token = req.cookies["auth-token"];
 
-    if (!token) {
-      return res.status(401).send("Access Denied");
-    }
+    // if (!token) {
+    //   return res.status(401).send("Access Denied");
+    // }
 
     try {
       const verified = jwt.verify(token, process.env.TOKEN_SECRET);
