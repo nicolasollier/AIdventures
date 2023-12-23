cfbbdc1f73186e29fb25ba958d6a1851c1ea428b
Nicolas Ollier <nicolas@lepetitmartin.com>
2023-12-23 21:39:29 +0100

    improves header styles

--- frontend/src/components/layout/header/Header.jsx
+++ frontend/src/components/layout/header/Header.jsx
@@ -1,28 +1,14 @@
-import { Box, Text } from "@chakra-ui/react";
 import SettingsDropdown from "../../dropdowns/settingsDropdown/SettingsDropdown";
+import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
-    <Box
-      w={"100%"}
-      zIndex={10}
-      position={"fixed"}
-      top={0}
-      p={"6"}
-      boxShadow="2xl"
-      bgColor={"gray.900"}
-    >
-      <Box
-        display={"flex"}
-        alignItems={"center"}
-        justifyContent="space-between"
-        maxW={"1280px"}
-        mx={"auto"}
-      >
-        <Text fontSize={"xl"}>MageTales</Text>
+    <div className={styles.header}>
+      <div className={styles.header__content}>
+        <h1 className={styles.brand}>MageTales</h1>
         <SettingsDropdown />
-      </Box>
-    </Box>
+      </div>
+    </div>
   );
 };
 
